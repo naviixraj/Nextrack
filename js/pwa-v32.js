1aa32b03f9541cfeadcc65cf5dbc5d69957b40ef
Naveen raj <naveenraj7756@gmail.com>
2026-07-20 22:00:24 +0530

    changing the version of the nextrack

--- js/pwa-v32.js
+++ js/pwa-v32.js
@@ -5,7 +5,7 @@
  */
 
 let deferredPrompt;
-const APP_VERSION = 'v57';
+const APP_VERSION = 'v105';
 
 // 1. Register Service Worker with a Static Version Buster
 if ('serviceWorker' in navigator) {
