8669b8f2336892c9a566e3ec4fd7b58e6247c0a5
Naveen raj <naveenraj7756@gmail.com>
2026-07-29 17:18:16 +0530

    feat: add premiumConfirm and premiumPrompt utilities

--- js/premium-alerts.js
+++ js/premium-alerts.js
@@ -24,3 +24,32 @@ window.alert = function(msg) {
     confirmButtonText: 'OK'
   });
 };
+
+window.premiumConfirm = async function(msg, isDestructive = false) {
+  const { isConfirmed } = await Swal.fire({
+    title: 'Are you sure?',
+    text: msg,
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: isDestructive ? '#ef4444' : '#3b82f6',
+    cancelButtonColor: '#374151',
+    confirmButtonText: 'Yes',
+    background: '#1f2937',
+    color: '#f3f4f6'
+  });
+  return isConfirmed;
+};
+
+window.premiumPrompt = async function(label, type = 'text', defaultValue = '') {
+  const { value } = await Swal.fire({
+    title: label,
+    input: type,
+    inputValue: defaultValue,
+    showCancelButton: true,
+    confirmButtonColor: '#3b82f6',
+    cancelButtonColor: '#374151',
+    background: '#1f2937',
+    color: '#f3f4f6'
+  });
+  return value;
+};
